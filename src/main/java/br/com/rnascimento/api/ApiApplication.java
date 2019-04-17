2683ba8cfe0b91acdf3e6894e889558b994aed2c
GPI - Rafael Boa Morte do Nascimento <P51801207@brasilprev.com.br>
2019-04-17 15:17:51 -0300

    Add travis-ci to the project

--- src/main/java/br/com/rnascimento/api/ApiApplication.java
+++ src/main/java/br/com/rnascimento/api/ApiApplication.java
@@ -1,11 +1,10 @@
 package br.com.rnascimento.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ApiApplication {
 
 	public static void main(String[] args) {
